580de7cbf7a3df98edc312c6406658509b6a9a2e
flyingace <dc@davidcameron.biz>
2017-10-18 01:50:19 -0400

    Added filtering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import { assign, isEmpty } from 'lodash';
 import { PhotoViewer } from './PhotoComponents/PhotoComponents';
 import './App.css';
 
@@ -9,8 +10,10 @@ class App extends Component {
     state = {
         'start': 0,
         'pristineData': [],
-        'sortableData': [],
-        'photoData': []
+        'mutableData': [],
+        'photoData': [],
+        'filters': { 'tag': '', 'source': '', 'org': '' },
+        'sortedBy': ''
     };
 
 
@@ -23,7 +26,7 @@ class App extends Component {
                     photoData.rows.forEach(function (row, idx) {
                         row.id = idx;
                     });
-                    this.setState({ 'pristineData': photoData.rows, 'sortableData': photoData.rows }, this.updatePhotoData);
+                    this.setState({ 'pristineData': photoData.rows, 'mutableData': photoData.rows }, this.updatePhotoData);
                 }
             )
     }
@@ -38,7 +41,7 @@ class App extends Component {
     forward = () => {
         const currentStart = this.state.start;
         const proposedStart = currentStart + 4;
-        const numberOfPhotos = this.state.sortableData.length;
+        const numberOfPhotos = this.state.mutableData.length;
         const newStart = (proposedStart < numberOfPhotos) ? proposedStart : currentStart;
         this.setState({ 'start': newStart }, this.updatePhotoData);
     };
@@ -46,15 +49,27 @@ class App extends Component {
     updatePhotoData = () => {
         const start = this.state.start;
         const end = start + 4;
-        const photosToRender = this.state.sortableData.slice(start, end);
+        const photosToRender = this.state.mutableData.slice(start, end);
         this.setState({ 'photoData': photosToRender });
+    };
+
+    sortData = () => {
+        const sortBy = this.state.sortedBy;
+        if (sortBy === '') {
+            this.updatePhotoData();
+        } else if (sortBy === 'date') {
+            this.sortByDate();
+        } else {
+            this.sortByActive();
+        }
+
     };
 
     //NOTE: moment.js does not like that one of the dates in your JSON file
     //is in the format DD/MM/YY and all the others are in the format DD/MM/YYYY
     //but it has agreed to work with it anyway. :P
     sortByDate = () => {
-        const sortedByDate = this.state.sortableData.sort(function (a, b) {
+        const sortedByDate = this.state.mutableData.sort(function (a, b) {
             const aDate = moment(a.date);
             const bDate = moment(b.date);
             if (aDate > bDate) {
@@ -66,11 +81,11 @@ class App extends Component {
             }
         });
 
-        this.setState({'sortedData': sortedByDate}, this.updatePhotoData);
+        this.setState({ 'mutableData': sortedByDate }, this.updatePhotoData);
     };
 
     sortByActive = () => {
-        const sortedByActive = this.state.sortableData.sort(function (a, b) {
+        const sortedByActive = this.state.mutableData.sort(function (a, b) {
             const aVal = (a.active === 'no') ? 1 : 0;
             const bVal = (b.active === 'no') ? 1 : 0;
             if (aVal > bVal) {
@@ -82,7 +97,51 @@ class App extends Component {
             }
         });
 
-        this.setState({'sortedData': sortedByActive}, this.updatePhotoData);
+        this.setState({ 'mutableData': sortedByActive }, this.updatePhotoData);
+    };
+
+    updateFilters = (target) => {
+        const filterBy = target.selectedOptions[0].value;
+        const filterCategory = target.name;
+
+        const newFilters = assign({}, this.state.filters, {
+            [filterCategory]: filterBy
+        });
+
+        this.setState({ 'filters': newFilters }, this.filterData);
+    };
+
+    mutateData = (evt) => {
+        const target = evt.target;
+        if (target.id === 'date' || target.id === 'active') {
+            this.setState({ 'sortedBy': target.id, 'start': 0 }, this.sortData);
+        } else {
+            //reset data before applying filters
+            this.setState({ 'mutableData': this.state.pristineData }, this.updateFilters(target));
+        }
+    };
+
+    filterData = () => {
+        const tagValue = this.state.filters['tag'];
+        const sourceValue = this.state.filters['source'];
+        const orgValue = this.state.filters['org'];
+
+
+        const filteredData = this.state.mutableData.filter((photoData) => {
+            return (
+                this.filterByCriteria('tag', photoData, tagValue) &&
+                this.filterByCriteria('status', photoData, sourceValue) &&
+                this.filterByCriteria('org', photoData, orgValue)
+            )
+        });
+
+        this.setState({ 'mutableData': filteredData }, this.sortData);
+    };
+
+    filterByCriteria = (filterType, photoData, valueToCompare) => {
+        if (valueToCompare === '') return true;
+
+        return photoData[filterType] === valueToCompare;
     };
 
 
@@ -96,39 +155,39 @@ class App extends Component {
                 </div>
                 <div className='controls'>
                     <div className='sorting-options'>
-                        <div className='button' id='date-sort' onClick={this.sortByDate}>Date</div>
-                        <div className='button' id='active-sort' onClick={this.sortByActive}>Active</div>
+                        <div className='button' id='date' onClick={this.mutateData}>Date</div>
+                        <div className='button' id='active' onClick={this.mutateData}>Active</div>
                     </div>
                     <div className='filtering-options'>
-                        <select>
-                            <option>Filter By Tag</option>
-                            <option>food</option>
-                            <option>simpsons</option>
-                            <option>the rock</option>
-                            <option>dogs</option>
-                            <option>comics</option>
-                            <option>SNL|RYAN GOSLING</option>
-                            <option>SNL</option>
-                            <option>cats|tacos</option>
+                        <select name="tag" id="tag" onChange={this.mutateData}>
+                            <option value=''>Filter By Tag</option>
+                            <option value='food'>food</option>
+                            <option value='simpsons'>simpsons</option>
+                            <option value='the rock'>the rock</option>
+                            <option value='dogs'>dogs</option>
+                            <option value='comics</option>'>comics</option>
+                            <option value='SNL|RYAN GOSLING'>SNL|RYAN GOSLING</option>
+                            <option value='SNL'>SNL</option>
+                            <option value='cats|tacos'>cats|tacos</option>
                         </select>
-                        <select>
-                            <option>Filter By Source</option>
-                            <option>ftp</option>
-                            <option>web</option>
-                            <option>dtu</option>
-                            <option>photomechanic</option>
+                        <select name="source" id="source" onChange={this.mutateData}>
+                            <option value=''>Filter By Source</option>
+                            <option value='ftp'>ftp</option>
+                            <option value='web'>web</option>
+                            <option value='dtu'>dtu</option>
+                            <option value='photomechanic'>photomechanic</option>
                         </select>
-                        <select>
-                            <option>Filter By Org</option>
-                            <option>ZCRVFR0ARO</option>
-                            <option>P6ENNB2RNO</option>
-                            <option>CSA49JNHUM</option>
-                            <option>9YCUF0FHYO</option>
-                            <option>6X90VJB294</option>
-                            <option>6X90VJB295</option>
-                            <option>72TEZ9GQI6</option>
-                            <option>72TEZ9GQI7</option>
-                            <option>72TEZ9GQI8</option>
+                        <select name="org" id="org" onChange={this.mutateData}>
+                            <option value=''>Filter By Org</option>
+                            <option value='ZCRVFR0ARO'>ZCRVFR0ARO</option>
+                            <option value='P6ENNB2RNO'>P6ENNB2RNO</option>
+                            <option value='CSA49JNHUM'>CSA49JNHUM</option>
+                            <option value='9YCUF0FHYO'>9YCUF0FHYO</option>
+                            <option value='6X90VJB294'>6X90VJB294</option>
+                            <option value='6X90VJB295'>6X90VJB295</option>
+                            <option value='72TEZ9GQI6'>72TEZ9GQI6</option>
+                            <option value='72TEZ9GQI7'>72TEZ9GQI7</option>
+                            <option value='72TEZ9GQI8'>72TEZ9GQI8</option>
                         </select>
                     </div>
                 </div>
